a1f31ce37911a75c81ad04c8d6adf6270fcba96a
MaksimMischenko <t89969534097@gmail.com>
2022-08-07 22:47:46 +0400

    +

--- src/main/java/com/example/pp_3_1_1_springboot/model/User.java
+++ src/main/java/com/example/pp_3_1_1_springboot/model/User.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @Column(name = "name")
     private String name;
